6c2beb1fd96793000fb17b67dd78757ae4025e1a
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-07-06 03:21:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+console.log("hello World")
+// //alert ch# 1
+// function submit(){
+//     // alert('Thanks for your feed back');
+    console.log('ch # 1 is completed')
+    console.log('ch # 2 is completed')
+    console.log('ch # 3 is completed')
+    console.log('ch # 4 is completed')
+    console.log('ch # 5 is completed')
+    console.log('ch # 6 is completed')
+    console.log('ch # 7 is completed')
+    console.log('ch # 8 is completed')
+    console.log('ch # 9 is completed')
+    // console.log('ch #  is completed')
+    // console.log('ch #  is completed')
+    // console.log('ch #  is completed')
+    // console.log('ch #  is completed')
+// } 
+//alert completed
+
+//2 Variables for Strings
+ 
+// var name = "ronaq";
+
+// alert(name)
+//completed
+
+//3 Variables for Numbers
+// var myWeigt = 50;
+// var ds = myWeigt + 100;
+// console.log(ds) 
+//completed
+
+//4 Variable Names Legal and Illegal
+//  use for camelCase (myName )
+// completed
+
+//5 Math expressions:Familiar operators
+
+// alert(2+2);
+// var minace = 10 % 1;
+// console.log(minace)
+// completed
+
+
+//6 Math expressions:Unfamiliar operators
+
+// var date = 2;
+// date--;
+// console.log(date)
+
+
+// var button = 1;
+// var newButton  = ++button;
+// console.log(newButton)
+
+// completed
+
+
+// 7 Math expressions:Eliminating ambiguity
+//  var num = 1 / 1  ;
+//  console.log(num)
+// completed
+
+
+// 8 Concatenating text strings 
+//  var book = "grade 1" ;
+//  var bookFound = " book is not found";
+//  alert(book + bookFound);
+//completed
+
+// 9 Prompts
+// var numberOfCats = prompt("How many cats?");
+//  var tooManyCats = numberOfCats + "";
+//  console.log("number of cats is "+tooManyCats);
+//completed
+
+// 10 if statements
+// var user = 150;
+// function red(){
+//     if(user > 140 ){
+// console.log("push ho gaya")
+// user++;
+
+
+// alert("bs karo"+ user )
+//     }
+// }
+//completed
+
